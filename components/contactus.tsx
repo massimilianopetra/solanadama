fbf8e7dbc9803c6c723fe1cda594cd112beb7dca
Massimiliano Petra <massimiliano.petra@gmail.com>
2024-05-16 18:47:24 +0200

    contact us

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -117,7 +117,7 @@ export default function Contactus() {
                 </div>
                 <div className="sm:col-span-2">
                   <label className="block mb-2 text-sm font-medium text-white">Your message</label>
-                  <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..." name="message" />
+                  <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..." name="message" />
                 </div>
 
                 <Button type={"submit"} variant="contained" className="bg-blue-500" endIcon={<SendIcon />}>
